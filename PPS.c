a9745a60d9237b6f24c7208ebc467825fcbb576f
PickleZapper <picklezapper@yahoo.com>
2015-04-08 15:23:26 -0700

    PPS.c

---
+++ PPS.c
@@ -0,0 +1,46 @@
+#pragma config(Sensor, in1,    firingAngle,    sensorPotentiometer)
+#pragma config(Sensor, in2,    firingAngleInput, sensorPotentiometer)
+#pragma config(Sensor, dgtl3,  firingSwitch,   sensorTouch)
+#pragma config(Motor,  port2,           elevationMotor, tmotorVex269_MC29, openLoop)
+#pragma config(Motor,  port3,           firingMotor,   tmotorVex393_MC29, openLoop)
+
+/*
+revisions:
+	sensor configurations for elevation up and down bumpers, ball detector, and elevation trigger have been removed
+		(however the firing switch remains in port 3)
+	elevation up and down bumper control code has been removed
+	rearranged firing angle movement code slightly
+	
+	delete this section after further revisions
+*/
+task main()
+{
+	while(true)
+	{
+		int angle = SensorValue(firingAngle);
+		while(abs(SensorValue(firingAngle)-SensorValue(firingAngleInput)/4) > 20){
+
+			int angleInput = SensorValue(firingAngleInput)/4;
+			if(SensorValue(firingAngle) < angleInput - 50)
+				startMotor(elevationMotor, 127);
+				
+			if(SensorValue(firingAngle) < angleInput)
+				startMotor(elevationMotor, 20);
+				
+			if(SensorValue(firingAngle) > angleInput + 50)
+				startMotor(elevationMotor, -100);
+
+			if(SensorValue(firingAngle) > angleInput)
+				startMotor(elevationMotor, -15);
+		}
+		stopMotor(elevationMotor);
+
+		while(SensorValue(firingSwitch) == 1){
+			startMotor(firingMotor, 127);
+		}
+
+		stopMotor(firingMotor);
+
+	}
+}
+
